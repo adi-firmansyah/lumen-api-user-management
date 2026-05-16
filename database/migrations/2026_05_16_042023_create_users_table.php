4543b94057d6f60704f8af78b8336132aa4c0374
Adi Firmansyah <adif4659@gmail.com>
2026-05-16 11:34:35 +0700

    Membuat seeder untuk dummy data user

--- database/migrations/2026_05_16_042023_create_users_table.php
+++ database/migrations/2026_05_16_042023_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('api_token');
+            $table->string('api_token')->nullable();
             $table->timestamps();
         });
     }
